efb1913c1fc0113c702533d4d9fed289d72f4e1f
AlexProstolucky <alexprostolucky@gmail.com>
2022-11-12 21:32:31 +0200

    Добавьте файлы проекта.

---
+++ map/map.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <map>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	multimap <string, string> mp{
+		{"always","завжди"},
+		{"never","нiколи"},
+		{"sometimes","деколи"},
+		{"maybe","можливо"},
+		{"work","робота"},
+		{"wear","одягати"},
+		{"expensive","дорогий"},
+		{"rock","камiнь"},
+		{"rock","колихати"},
+		{"pen","ручка"}
+	};
+
+	for (auto i : mp)
+	{
+		cout << i.first << " " << i.second << endl;
+	}
+
+}
